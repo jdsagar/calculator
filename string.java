03104cf68b3e001e316c7ecf2a4604ea97a29daf
jdsagar <jdsagar18@gmail.com>
2022-04-29 21:30:16 +0530

    string

---
+++ string.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class string {
+
+	public static void main(String[] args){
+		Scanner scan= new Scanner(System.in);
+		System.out.println("Enter the string based input:");
+		String s1 = scan.next();
+		System.out.println("The string is : "+s1);
+		int i =Integer.parseInt(s1);
+		System.out.println("Converted string is:"+ i);
+		
+		
+		
+		
+		
+
+	}
+
+}
